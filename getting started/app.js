b3cb95121c032b2d0719ec552eae9099a02df497
shiven1703 <shiven1703@gmail.com>
2020-09-21 23:10:06 +0530

    Updated project structure

--- getting started/app.js
+++ getting started/app.js
@@ -1,48 +1,10 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((request, response) => {
-  const urlPath = request.url;
-  const requestType = request.method;
-
-  if (urlPath === '/') {
-    let loginFormHtml = getLoginForm();
-    response.setHeader('Content-Type', 'text/html');
-    response.write(loginFormHtml);
-    return response.end();
-
-  } else if (urlPath === '/auth' && requestType === 'POST') {
-
-  	let data = [];
-
-  	request.on('data', (chunk) => {
-  		data.push(chunk);
-  	});
-
-  	request.on('end', () => {
-  		const parsedData = Buffer.concat(data).toString();
-  		fs.writeFile('data.txt', parsedData, (error) => {
-  			response.statusCode = 200;
-		  	response.setHeader('Content-Type', 'text/html');
-		  	response.write('<h1>Your data has been recorded.</h1>');
-		  	response.end();
-  		});
-  		
-  	});
-  }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(5000, () => {
   console.log('Server is running...');
 });
 
-function getLoginForm() {
-  return `<html>
-	 		<head><title>Login</title></head>
-		 	<body><form action="/auth" method="POST">
-		 	<label>Username: </label> <input type="text" name="username" required/><br/>
-		 	<label>Password: </label> <input type="password" name="password" required/><br/>
-		 	<input type="submit" value="Login"/>
-		 	</form></body>
-		 	</html>`;
-}
+
