cdbb95f1c5ef751a2a9616e69b79945de528a1c5
shiven1703 <shiven1703@gmail.com>
2020-09-26 20:58:43 +0530

    Models

--- shopApp/Models/product.js
+++ shopApp/Models/product.js
@@ -1,8 +1,54 @@
+const path = require("path");
+const fs = require("fs");
+
+const rootDirName = require("../utils/path");
+
+const getProductsFromFile = () => {
+  return new Promise((resolve, rejects) => {
+    fs.readFile(
+      path.join(rootDirName, "data", "products.json"),
+      (err, data) => {
+        let products = [];
+        if (!err) {
+          products = JSON.parse(data);
+        }
+        resolve(products);
+      }
+    );
+  });
+};
+
+const saveProductsToFile = (products) => {
+  return new Promise((resolve, rejects) => {
+    fs.writeFile(
+      path.join(rootDirName, "data", "products.json"),
+      JSON.stringify(products),
+      (err) => {
+        resolve();
+      }
+    );
+  });
+};
+
 class Product {
   constructor(productName, productDescription, price) {
     this.productName = productName;
     this.productDescription = productDescription;
     this.price = price;
+  }
+
+  async save() {
+    return new Promise(async (resolve, rejects) => {
+      let existingProducts = await getProductsFromFile();
+      existingProducts.push(this);
+      resolve(existingProducts);
+    }).then(async (existingProductList) => {
+      await saveProductsToFile(existingProductList);
+    });
+  }
+
+  static async getAllProducts() {
+    return await getProductsFromFile();
   }
 }
 
--- shopApp/Models/user.js
+++ shopApp/Models/user.js
@@ -1,8 +1,63 @@
+const path = require("path");
+const fs = require("fs");
+
+const rootDirName = require("../utils/path");
+
+const getAllUsersFromFile = () => {
+  return new Promise((resolve, rejects) => {
+    fs.readFile(path.join(rootDirName, "data", "users.json"), (err, data) => {
+      let users = [];
+      if (!err) {
+        users = JSON.parse(data);
+      }
+      resolve(users);
+    });
+  });
+};
+
+const writeAllUsersToFile = (users) => {
+  return new Promise((resolve, rejects) => {
+    fs.writeFile(
+      path.join(rootDirName, "data", "users.json"),
+      JSON.stringify(users),
+      (err) => {
+        resolve();
+      }
+    );
+  });
+};
+
 class User {
   constructor(name, email, password) {
     this.name = name;
     this.email = email;
     this.password = password;
+  }
+
+  async save() {
+    return new Promise(async (resolve, rejects) => {
+      let existingUsers = await getAllUsersFromFile();
+      existingUsers.push(this);
+      resolve(existingUsers);
+    }).then(async (updatedUserList) => {
+      await writeAllUsersToFile(updatedUserList);
+    });
+  }
+
+  static async authenticate(email, password) {
+    let isFound = false;
+    const userList = await getAllUsersFromFile();
+    for (let user of userList) {
+      if (user.email === email && user.password === password) {
+        isFound = true;
+        break;
+      }
+    }
+    return isFound;
+  }
+
+  static async getAllUsers() {
+    return await getAllUsersFromFile();
   }
 }
 
