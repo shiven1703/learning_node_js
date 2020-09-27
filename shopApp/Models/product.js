1045b24fbc4407b9c2be4aa73052177e9bdd8eb4
shiven1703 <shiven1703@gmail.com>
2020-09-27 22:56:23 +0530

    Product Model

--- shopApp/Models/product.js
+++ shopApp/Models/product.js
@@ -3,6 +3,57 @@ const fs = require("fs");
 
 const rootDirName = require("../utils/path");
 
+class Product {
+  constructor(productName, productDescription, price) {
+    this.productId = getRandomNumber(0, 1000);
+    this.productName = productName;
+    this.productDescription = productDescription;
+    this.price = price;
+  }
+
+  async save() {
+    return new Promise(async (resolve, rejects) => {
+      let existingProducts = await getProductsFromFile();
+      existingProducts.push(this);
+      resolve(existingProducts);
+    }).then(async (existingProductList) => {
+      await saveProductsToFile(existingProductList);
+    });
+  }
+
+  static async update(pid, productName, productDescription, price) {
+    const products = await getProductsFromFile();
+    const updatedProducts = products.map((product) => {
+      if (product.productId === pid) {
+        product.productName = productName;
+        product.productDescription = productDescription;
+        product.price = price;
+      }
+      return product;
+    });
+    await saveProductsToFile(updatedProducts);
+  }
+
+  static async delete(pid) {
+    const products = await getProductsFromFile();
+    const updatedProducts = products.filter((product) => {
+      if (product.productId !== pid) {
+        return true;
+      }
+    });
+    await saveProductsToFile(updatedProducts);
+  }
+
+  static async getProductById(pid) {
+    let products = await getProductsFromFile();
+    return products.find((product) => product.productId === pid);
+  }
+
+  static async getAllProducts() {
+    return await getProductsFromFile();
+  }
+}
+
 const getProductsFromFile = () => {
   return new Promise((resolve, rejects) => {
     fs.readFile(
@@ -30,26 +81,8 @@ const saveProductsToFile = (products) => {
   });
 };
 
-class Product {
-  constructor(productName, productDescription, price) {
-    this.productName = productName;
-    this.productDescription = productDescription;
-    this.price = price;
-  }
-
-  async save() {
-    return new Promise(async (resolve, rejects) => {
-      let existingProducts = await getProductsFromFile();
-      existingProducts.push(this);
-      resolve(existingProducts);
-    }).then(async (existingProductList) => {
-      await saveProductsToFile(existingProductList);
-    });
-  }
-
-  static async getAllProducts() {
-    return await getProductsFromFile();
-  }
-}
+const getRandomNumber = (start, end) => {
+  return Math.floor(Math.random() * (start - end + 1)) + end;
+};
 
 module.exports = Product;
