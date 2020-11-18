e082fd2cd2553f6270940a027e0078b753405f33
shiven1703 <shiven1703@gmail.com>
2020-11-18 13:57:45 +0530

    added mongodb connection

--- shopApp/Models/user.js
+++ shopApp/Models/user.js
@@ -1,26 +1,34 @@
-const Sequelize = require("sequelize");
+const { getDb } = require("./../utils/database");
 
-const db = require("./../utils/database");
+class User {
+  constructor(name, password, email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
 
-const User = db.define("user", {
-  userId: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  async save() {
+    try {
+      const collection = getDb().collection("users");
+      await collection.insertOne(this);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async authenticate(userEmail, userPassword) {
+    try {
+      const collection = getDb().collection("users");
+      console.log(userEmail, userPassword);
+      const user = collection.findOne({ email: userEmail, password: userPassword });
+      return user;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 module.exports = User;
+
+
