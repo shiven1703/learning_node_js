9ce86ca7aae7d923a61bb5ff4fdc3e79624f3b47
shiven1703 <shiven1703@gmail.com>
2020-11-18 13:59:30 +0530

    config setup for mongodb

--- shopApp/utils/database.js
+++ shopApp/utils/database.js
@@ -1,8 +1,23 @@
-const Sequelize = require("sequelize");
+const mongoClient = require("mongodb").MongoClient;
 
-const db = new Sequelize("shop", "root", "Letmein@123", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const dbURL = "mongodb://localhost:27017";
+const dbName = "shop";
 
-module.exports = db;
+let _dbInstance;
+
+exports.mongoConnect = () => {
+  return new Promise((reslove, reject) => {
+    mongoClient.connect(dbURL).then((client) => {
+      _dbInstance = client.db(dbName);
+      reslove();
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+};
+
+exports.getDb = () => {
+  if (_dbInstance) {
+    return _dbInstance;
+  }
+};
